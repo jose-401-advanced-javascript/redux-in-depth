7f676705eddbb6c76aef67f360ae13019e75dcd1
Jose <j.a.ojeda@live.com>
2019-11-12 15:00:40 -0800

    corrected reducer

--- src/store.js
+++ src/store.js
@@ -11,7 +11,7 @@ function reducer(state = initialState, action) {
   switch(action.type) {
     case 'DRINK_COFFEE':
       return { ...state, coffees: state.coffees + 1 };
-    case 'EAT_SNACKS':
+    case 'EAT_SNACK':
       return { ...state, snacks: state.snacks + 1 };
     case 'TAKE_NAP':
       return { ...state, naps: state.naps + 1 };
