629aeaaf88485827a380db7cc4080f390626a28b
Jose <j.a.ojeda@live.com>
2019-11-12 16:39:04 -0800

    created index.js in reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import mood from './moodReducer';
+
+export default combineReducers({
+  mood
+});
--- src/selectors/moodSelectors.js
+++ src/selectors/moodSelectors.js
@@ -1,7 +1,7 @@
-export const getCoffee = state => state.coffees;
-export const getSnack = state => state.snacks;
-export const getNap = state => state.naps;
-export const getStudies = state => state.studies;
+export const getCoffee = state => state.mood.coffees;
+export const getSnack = state => state.mood.snacks;
+export const getNap = state => state.mood.naps;
+export const getStudies = state => state.mood.studies;
 
 export const actions = [
   { name: 'DRINK_COFFEE', text: 'Drink Coffee', stateName: 'coffees', selector(state) {return getCoffee(state);}  },
@@ -10,10 +10,10 @@ export const actions = [
   { name: 'STUDY', text: 'Study', stateName: 'studies', selector(state) {return getStudies(state);} },
 ];
 
-export const isTired = state => state.coffees < 1 && state.naps < 1;
-export const isHyper = state => state.coffees > 3;
-export const isEducated = state => state.studies > 2;
-export const isHungry = state => state.snacks < 1;
+export const isTired = state => state.mood.coffees < 1 && state.mood.naps < 1;
+export const isHyper = state => state.mood.coffees > 3;
+export const isEducated = state => state.mood.studies > 2;
+export const isHungry = state => state.mood.snacks < 1;
 
 export const getFace = state => {
   if(isTired(state) && isHungry(state)) return '🤬';
