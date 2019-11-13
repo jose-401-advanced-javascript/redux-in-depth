7bfdd8eda3c44bebf2bbc03e45aaca2b906ff2bb
Jose <j.a.ojeda@live.com>
2019-11-12 17:32:31 -0800

    created savesReducer.js

---
+++ src/reducers/savesReducer.js
@@ -0,0 +1,9 @@
+
+export default function reducer(state = [], action) {
+  switch(action.type) {
+    case 'SAVE_GAME':
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+}
